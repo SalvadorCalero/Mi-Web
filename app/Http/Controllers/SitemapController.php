d0cf1ee617374ee80382839c5bc96a33ce519224
SalvadorCalero <salkalero@gmail.com>
2026-08-21 21:06:05 +0200

    Archivos incompletos, se actualizan.

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Project;
+use Illuminate\Http\Response;
 
 class SitemapController extends Controller
 {
-    public function index()
+    public function index(): Response
     {
-        header('Content-Type', 'text/xml');
+        // Recuperamos los proyectos por si en el futuro quieres iterar sus URLs en el sitemap
+        $projects = Project::all();
+
+        // Renderizamos la vista de Blade que contiene la estructura XML
+        $content = view('sitemap', compact('projects'));
+
+        // Retornamos la respuesta asegurando la cabecera 'text/xml' que espera Google
+        return response($content, 200)->header('Content-Type', 'text/xml');
     }
 }
--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,16 +1,18 @@
 {!! '<' . '?' . 'xml version="1.0" encoding="UTF-8"?' . '>' !!}
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <!-- Página Principal -->
+    <url>
+        <loc>https://caleroestudio.com/</loc>
+        <lastmod>{{ now()->tz('UTC')->toAtomString() }}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+    </url>
 
-    
-    
-        https://caleroestudio.com/
-        {{ now()->tz('UTC')->toAtomString() }}
-        weekly
-        1.0
-    
-
-    
-    
-        https://caleroestudio.com/portfolio
-        {{ now()->tz('UTC')->toAtomString() }}
-        weekly
-        0.8
+    <!-- Sección/Página Portfolio -->
+    <url>
+        <loc>https://caleroestudio.com/portfolio</loc>
+        <lastmod>{{ now()->tz('UTC')->toAtomString() }}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
+</urlset>
